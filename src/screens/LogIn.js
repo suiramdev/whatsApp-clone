6f72ae4ff9df6148763cb1e5f114d08eddecbc62
HamsterOh <mariusnouchet@gmail.com>
2020-11-07 17:04:00 +0100

    Error message using Alert MUI, License MD and README changes

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './LogIn.scss';
+import { Alert } from '@material-ui/lab';
 import { ArrowBack, AlternateEmail, Lock, WhatsApp } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import firebase from 'services/firebase';
 
 class LogIn extends Component {
+    state = {}
+
     constructor(props) {
         super(props);
         this.emailInput = React.createRef();
@@ -21,13 +24,9 @@ class LogIn extends Component {
         const passwordInput = this.passwordInput.current;
 
         firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value)
-        .then(function() {
-            emailInput.value = "";
-            passwordInput.value = "";
-        })
         .catch(function(error) {
-            console.log(error.code + " : " + error.message);
-        });
+            this.setState({error: error.message});
+        }.bind(this));
     }
 
     render() {
@@ -36,6 +35,7 @@ class LogIn extends Component {
                 <Link className="login__return" to="/"><ArrowBack /></Link>
                 <form className="login__form" onSubmit={this.handleSubmit}>
                     <WhatsApp className="login__form-logo"/>
+                    {this.state.error ? (<Alert severity="error">{this.state.error}</Alert>) : null}
                     <div className="login__form-input">
                         <AlternateEmail />
                         <input ref={this.emailInput} type="text" placeholder="Email" required></input>
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './Register.scss';
-import { AlternateEmail, Lock, WhatsApp } from '@material-ui/icons';
+import { Alert } from '@material-ui/lab';
+import { AlternateEmail, Lock, WhatsApp, ErrorOutline } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 import firebase from 'services/firebase';
 
 class Register extends Component {
+    state = {}
+
     constructor(props) {
         super(props);
         this.emailInput = React.createRef();
@@ -13,36 +16,29 @@ class Register extends Component {
 
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    componentDidMount() {
-        window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
-            'callback': (response) => {
-                window.captchaVerified = true;
-            },
-            'expired-callback': () => {
-                window.captchaVerified = false;
-            }
-        });
-        window.recaptchaVerifier.render();
-    }
     
     handleSubmit(event) {
         event.preventDefault();
 
-        if (!window.captchaVerified) { return; } // Captha non verified
         const emailInput = this.emailInput.current;
         const passwordInput = this.passwordInput.current;
         const confirmPasswordInput = this.confirmPasswordInput.current;
-        if (passwordInput.value != confirmPasswordInput.value) { return; } // Confirm password incorrect
+        
+        if (passwordInput.value != confirmPasswordInput.value) { 
+            this.setState({error: "The passwords mismatch"})
+            return;
+        } // Confirm password incorrect
         
         firebase.auth().createUserWithEmailAndPassword(emailInput.value, passwordInput.value)
         .then(function() {
-            emailInput.value = "";
-            passwordInput.value = "";
-            confirmPasswordInput.value = "";
+            this.setState({success: 'You have signed up successfully'});
+
+            emailInput.value = '';
+            passwordInput.value = '';
+            confirmPasswordInput.value = '';
         })
         .catch(function(error) {
-            console.log(error.code + " : " + error.message); // Could not login
+            this.setState({error: error.message});
         });
     }
 
@@ -51,6 +47,8 @@ class Register extends Component {
             <div className="register">
                 <form className="register__form" onSubmit={this.handleSubmit}>
                     <WhatsApp className="register__form-logo"/>
+                    {this.state.error ? (<Alert severity="error">{this.state.error}</Alert>) : null}
+                    {this.state.success ? (<Alert severity="success">{this.state.success}</Alert>) : null}
                     <div className="register__form-input">
                         <AlternateEmail />
                         <input ref={this.emailInput} type="email" placeholder="Email" required></input>
@@ -63,9 +61,9 @@ class Register extends Component {
                         <Lock />
                         <input ref={this.confirmPasswordInput} type="password" placeholder="Confirm password" required></input>
                     </div>
-                    <Link className="register__form-more" to="/LogIn">I already have an account</Link>
-                    <div id="recaptcha-container" style={{marginTop: 2+"rem"}}></div>
+                    <div id="recaptcha-container"></div>
                     <input className="register__form-submit" type="submit" value="SIGN UP" />
+                    <Link className="register__form-more" to="/LogIn">I already have an account</Link>
                 </form>
             </div>
         );
