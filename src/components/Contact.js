f74d1eda5303e155c2511120eea1ed2a6b581fcb
suiramdev <mariusnouchet@gmail.com>
2021-03-22 14:08:41 +0100

    Code cleanup Added contactList

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,17 +3,16 @@ import 'components/Contact.scss';
 import { Avatar } from '@material-ui/core';
 
 class Contact extends Component {
-    state = {  }
     render() { 
         return (
-            <a className="contact">
+            <button className="contact">
                 <Avatar/>
                 <div style={{marginLeft: 0.5+"rem"}}>
                     <h1 className="contact__name">Contact name</h1>
                     <p className="contact__description">The last message sent goes here</p>
                 </div>
                 <p className="contact__time">12:00:50</p>
-            </a>
+            </button>
         );
     }
 }
