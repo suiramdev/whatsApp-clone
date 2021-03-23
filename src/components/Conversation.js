2352591ecc17ee426b149b6ff497a3eb84e91804
suiramdev <mariusnouchet@gmail.com>
2021-03-23 17:33:12 +0100

    Send message (not finished yet) Snapshot of messages

--- src/components/Conversation.js
+++ src/components/Conversation.js
@@ -4,28 +4,59 @@ import { Avatar } from '@material-ui/core';
 import SendRoundedIcon from '@material-ui/icons/SendRounded';
 import SentimentVerySatisfiedRoundedIcon from '@material-ui/icons/SentimentVerySatisfiedRounded';
 import firebase, {firestore} from "../services/firebase";
+import {useHistory} from "react-router";
 
 class Conversation extends Component {
     constructor(props) {
         super(props);
 
+        this.message = React.createRef();
+
         this.state = {
             contact: [],
+            conversationId: "",
             conversation: []
         }
+
+        this.sendMessage = this.sendMessage.bind(this);
     }
 
     componentDidMount() {
-        firestore.collection("users").doc(firebase.auth().currentUser.uid).collection("contacts")
-            .doc(this.props.match.params.uid).get()
+        // Get contact
+        firestore.collection("users").doc(firebase.auth().currentUser.uid).collection("contacts").get()
             .then(function(querySnapshot) {
-                this.setState({
-                    contact: querySnapshot.docs[0].data()
-                });
+                let contact;
+                querySnapshot.forEach(doc => {
+                    if (doc.data().user.id == this.props.match.params.uid) contact = doc;
+                })
+
+                if (contact) {
+                    this.setState({contact: contact});
+                } else {
+                    this.props.history.goBack();
+                }
+            }.bind(this));
+
+        // Get conversation and retrieve changes
+        firestore.collection("conversations")
+            .where("participants", "array-contains-any", [firebase.auth().currentUser.uid, this.props.match.params.uid])
+            .onSnapshot(function(querySnapshot) {
+                this.setState({conversation: querySnapshot.docs[0].data()});
             }.bind(this));
     }
 
-    render() { 
+    sendMessage() {
+        firestore.collection("conversations").doc(this.state.conversationId).set({
+            messages: [
+                {
+                    content: this.message.current.value,
+                    from: firebase.auth().currentUser.uid
+                }
+            ]
+        }, {merge: true});
+    }
+
+    render() {
         return (
             <div className="main">
                 <div className="main__header">
@@ -33,38 +64,26 @@ class Conversation extends Component {
                     <h1 className="main__header-title">{this.state.contact.name || "New contact"}</h1>
                 </div>
                 <div className="main__chat">
-                    <div className="main__chat-message">
-                        <Avatar />
-                        <div className="main__chat-messageContent">
-                            <p>Lorem ipsum dolor sit amet, consectetur.</p>
+                    {this.state.conversation.messages && this.state.conversation.messages.map(message => (
+                        <div key={message} className={`main__chat-message ${message.from == firebase.auth().currentUser.uid ? "me" : ""}`}>
+                            <Avatar />
+                            <div className="main__chat-messageContent">
+                                <p>{message.content}</p>
+                            </div>
+                            <p className="main__chat-messageTime">12:08:09</p>
                         </div>
-                        <p className="main__chat-messageTime">12:08:09</p>
-                    </div>
-                    <div className="main__chat-message me">
-                        <Avatar />
-                        <div className="main__chat-messageContent">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis ullamcorper ipsum. Praesent id dapibus est. Morbi velit ipsum, semper sed libero venenatis, imperdiet bibendum ipsum. Fusce pulvinar mauris turpis, non faucibus ipsum luctus in. Praesent varius maximus enim. Sed auctor lectus ac quam dapibus, eu tincidunt odio iaculis. Cras congue malesuada neque in iaculis. Etiam varius efficitur sollicitudin. Nullam sagittis diam ipsum, sed ornare nunc tincidunt porta. Donec aliquet ex non tincidunt pharetra.</p>
-                        </div>
-                        <p className="main__chat-messageTime">12:08:09</p>
-                    </div>
-                    <div className="main__chat-message">
-                        <Avatar />
-                        <div className="main__chat-messageContent">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi quis ullamcorper ipsum. Praesent id dapibus est. Morbi velit ipsum, semper sed libero venenatis, imperdiet bibendum ipsum. Fusce pulvinar mauris turpis, non faucibus ipsum luctus in. Praesent varius maximus enim. Sed auctor lectus ac quam dapibus, eu tincidunt odio iaculis. Cras congue malesuada neque in iaculis. Etiam varius efficitur sollicitudin. Nullam sagittis diam ipsum, sed ornare nunc tincidunt porta. Donec aliquet ex non tincidunt pharetra.</p>
-                        </div>
-                        <p className="main__chat-messageTime">12:08:09</p>
-                    </div>
+                    ))}
                 </div>
                 <div className="main__footer">
                     <div className="main__footer-entry">
-                        <input type="text" placeholder="Type your message" />
+                        <input ref={this.message} type="text" placeholder="Type your message" />
                         <button><SentimentVerySatisfiedRoundedIcon /></button>
                     </div>
-                    <button><SendRoundedIcon /></button>
+                    <button onClick={this.sendMessage}><SendRoundedIcon /></button>
                 </div>
             </div>
         );
     }
 }
  
-export default Conversation;
+export default (props) => <Conversation {...props} history={useHistory()} />;
